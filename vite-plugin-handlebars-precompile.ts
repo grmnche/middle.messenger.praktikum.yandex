3f8673146656212ecd9832f179d25818eaafdeb5
gingerwnshp <grmnche@yandex.ru>
2023-12-13 18:27:55 +0300

    fix

--- vite-plugin-handlebars-precompile.ts
+++ vite-plugin-handlebars-precompile.ts
@@ -1,4 +1,4 @@
-import {PluginOption} from 'vite';
+import { PluginOption } from 'vite';
 import Handlebars from 'handlebars';
 
 export default function vitePluginHandlebarsPrecompile(): PluginOption {
@@ -6,14 +6,14 @@ export default function vitePluginHandlebarsPrecompile(): PluginOption {
         name: 'vite-plugin-handlebars-precompile',
         transform(code, id) {
             if (id.endsWith('.hbs') || id.endsWith('.handlebars')) {
+                const precompiled = Handlebars.precompile(code);
                 return {
-                    code: `
-                        import Handlebars from 'handlebars';
-                    
-                        export default Handlebars.template(${Handlebars.precompile(code)});    
-                    `,
-                }
+                    code: `import Handlebars from 'handlebars';
+                        export default Handlebars.template(${precompiled});`,
+                    map: null
+                };
             }
+            return undefined;
         }
-    }
+    };
 }
